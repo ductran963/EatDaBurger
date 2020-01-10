494c225e5d6b1fc76be2dc39babe45b17df49ead
Duc T <ductran963@gmail.com>
2020-01-10 15:58:31 -0800

    add option worked, to be devour and devoured worked

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+insert into burgers (burger_name, devoured) 
+values ("Big Mac", false),
+("Hamburger", false),
+("Western Bacon Cheese Burger", false);
---
+++ models/burger.js
@@ -0,0 +1,22 @@
+//Import necessary files so we can get access to all the data
+var orm = require("../config/orm.js");
+
+var burger = {
+    select: function(cb){
+        orm.selectAll("burgers", function(res){
+            cb(res);
+        });
+    },
+    insert: function(burger_name, devoured, cb){
+        orm.insertOne(burger_name, devoured, function(res){
+            cb(res)
+        });
+    },
+    update: function(devoured, condition, cb){
+        orm.updateOne("burgers", devoured, condition, function(res){
+            cb(res)
+        })
+    }
+}
+
+module.exports = burger;
